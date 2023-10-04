4bcb0dff0555c95c972ef53273125ce8fb1a9290
onyxgeek <onyxpeek@gmail.com>
2023-10-04 09:09:22 +0300

    feat: add the right solution

--- Course 1/homework1.java
+++ Course 1/homework1.java
@@ -1,9 +1,10 @@
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-      int x = 24;
-       String y = "SOS";
-       System.out.printf("Numarul variabilei X  %nIar sirul de caractere pentru Y este %s",x, y);
+       int x = 24;
+        String y = "SOS";
+        System.out.printf("Numarul variabilei X: %d %nIar sirul de caractere pentru Y este: %s", x, y);
+
 
     }
 }
