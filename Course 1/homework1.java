5b534dcbcae3af276f15e81c7c045e809b9a8c62
onyxgeek <onyxpeek@gmail.com>
2023-10-04 09:10:02 +0300

    fix: add comments for better understanding

--- Course 1/homework1.java
+++ Course 1/homework1.java
@@ -1,7 +1,9 @@
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-       int x = 24;
+        // %d specific format for integer
+        // %s specific format for string
+        int x = 24;
         String y = "SOS";
         System.out.printf("Numarul variabilei X: %d %nIar sirul de caractere pentru Y este: %s", x, y);
 
