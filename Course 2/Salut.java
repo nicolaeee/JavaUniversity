4bba9ad71063e6e3d27bdfa387711e8de948ef04
onyxgeek <onyxpeek@gmail.com>
2023-10-10 12:25:10 +0300

    feat: add the part of the solution

--- Course 2/Salut.java
+++ Course 2/Salut.java
@@ -1,3 +1,27 @@
-public class Salut {
+import javax.swing.JOptionPane;
 
+class Salut {
+	public static void main(String args[]){
+		String inputValueOne = JOptionPane.showInputDialog("Introdu un numar dorit: ");
+		String inputValueTwo = JOptionPane.showInputDialog("Introdu al doilea numar dorit: ");
+		String inputValueThree = JOptionPane.showInputDialog("Care ar fi suma acestor doua numere?: ");
+		int numOne = Integer.parseInt(inputValueOne);
+		int numTwo = Integer.parseInt(inputValueTwo);
+		int guessResult = Integer.parseInt(inputValueThree);
+		int rightResult = numOne + numTwo;
+		String showMessage = "Nu este corect";
+
+		if(guessResult == rightResult){
+
+			JOptionPane.showMessageDialog(null, numOne + numTwo);
+
+		} else {
+
+			JOptionPane.showMessageDialog(null, showMessage);
+
+
+  			}
+
+	System.out.println("Hiiii");
+	}
 }
