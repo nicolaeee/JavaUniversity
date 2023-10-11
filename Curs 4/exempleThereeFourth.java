68832f420186199060129df429a2b0daf8a66b25
onyxgeek <onyxpeek@gmail.com>
2023-10-11 21:05:10 +0300

    feat: add another exemple

---
+++ Curs 4/exempleThereeFourth.java
@@ -0,0 +1,33 @@
+
+public class exempleThereeFourth {
+    public static void main(String[] args){
+        byte a = 2;
+        byte b = 126;
+        System.out.println(a * b + "\n");
+
+        short c = 2000;
+        short d = 23123;
+        System.out.println(c * d + "\n");
+
+        char e = 's';
+        char l = '2';
+        System.out.println(e * l + "\n");
+
+        int h = 12121;
+        int m = 121211414;
+        System.out.println(h * m + "\n");
+
+        long x = 12121414;
+        long z = 1134;
+        System.out.println(x * z + "\n");
+
+        float o = 1212;
+        float r = 212;
+        System.out.println(o * r + "\n");
+
+        double v = 2.2;
+        double y = 31.4;
+        System.out.println(v + y + "\n");
+
+   }
+}
