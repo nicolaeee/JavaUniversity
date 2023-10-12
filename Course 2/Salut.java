5b316cb1a261038fbe5173e95a270e348b23c2df
onyxgeek <onyxpeek@gmail.com>
2023-10-12 18:51:46 +0300

    feat: add the final part of my very first GUI app

--- Course 2/Salut.java
+++ Course 2/Salut.java
@@ -10,17 +10,29 @@ class Salut {
 		int guessResult = Integer.parseInt(inputValueThree);
 		int rightResult = numOne + numTwo;
 		String showMessageWrong = "Nu este corect, mai incearca odata!";
+		String showMessageRightOne = "Bravo, ai reusit, raspunsul tau: ";
+		String showMessageRightTwo = " este absolut corect ";
+
+
+		while(guessResult != rightResult){
+
+
+		 JOptionPane.showMessageDialog(null, showMessageWrong);
+         inputValueOne = JOptionPane.showInputDialog("Introdu un numar dorit: ");
+		 inputValueTwo = JOptionPane.showInputDialog("Introdu al doilea numar dorit: ");
+		 inputValueThree = JOptionPane.showInputDialog("Care ar fi suma acestor doua numere?: ");
+		 numOne = Integer.parseInt(inputValueOne);
+		 numTwo = Integer.parseInt(inputValueTwo);
+		 guessResult = Integer.parseInt(inputValueThree);
+		 rightResult = numOne + numTwo;
+
+		}
 
 		if(guessResult == rightResult){
 
-			JOptionPane.showMessageDialog(null, numOne + numTwo);
+			JOptionPane.showMessageDialog(null, showMessageRightOne + (numOne + numTwo) + showMessageRightTwo);
 
-		} else {
-
-			JOptionPane.showMessageDialog(null, showMessageWrong);
-
-
-  			}
+		}
 
 	}
 }
