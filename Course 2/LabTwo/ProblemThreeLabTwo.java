eafd152004012d88753cb4ce551197df2481e25b
onyxgeek <onyxpeek@gmail.com>
2023-10-23 14:08:04 +0300

    fix: delete the unused space

--- Course 2/LabTwo/ProblemThreeLabTwo.java
+++ Course 2/LabTwo/ProblemThreeLabTwo.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 public class ProblemThreeLabTwo {
     public static void main(String args[]){
         String inputValueOne = JOptionPane.showInputDialog("Introdu primul numar ");
-	String inputValueTwo = JOptionPane.showInputDialog("Introdu al doile numar  ");
+	    String inputValueTwo = JOptionPane.showInputDialog("Introdu al doile numar  ");
         String inputValueThree = JOptionPane.showInputDialog("Introdu al treilea  numar  ");
         int numOne = Integer.parseInt(inputValueOne);
         int numTwo = Integer.parseInt(inputValueTwo);
@@ -14,10 +14,5 @@ public class ProblemThreeLabTwo {
         String arrayString = Arrays.toString(myArray);
         String message = " sunt numerele introduse de tine si sortate in mod crescator";
         JOptionPane.showMessageDialog(null, arrayString + message);
-
-
-
-
-
     }
 }
